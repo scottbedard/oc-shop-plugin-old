741bc54d0d6983b4d15a1a8433fde379e2de029a
scottbedard <scottbedard90@gmail.com>
2015-07-09 12:52:20 -0400

    Doc and param fix

--- traits/NumericColumnTrait.php
+++ traits/NumericColumnTrait.php
@@ -12,11 +12,11 @@ trait NumericColumnTrait {
      * @param   string  $left       The left side of the condition
      * @param   string  $then       The value to use if the condition is true
      * @param   string  $else       The value to use if the condition is false
-     * @param   string  $status     The column alias
-     * @param   mixed   $right      The right side of the condition
+     * @param   string  $as         The column alias
+     * @param   sting   $right      The right side of the condition
      * @return  DB
      */
-    public function selectNumeric($table, $left, $then, $else, $as = 'amount', $right = 1)
+    public function selectNumeric($table, $left, $then, $else, $as = 'amount', $right = '1')
     {
         return DB::raw("(CASE WHEN `$table`.`$left` = $right THEN `$table`.`$then` ELSE `$table`.`$else` END) as `$as`");
     }
